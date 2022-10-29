03c6a02906049171c5ad400c933a0292edd43415
Matias Paz <matiaspazconti2020@gmail.com>
2022-10-29 19:11:30 -0300

    TG-05: Updated admin index

--- src/Components/Admins/index.js
+++ src/Components/Admins/index.js
@@ -1,9 +1,24 @@
+import { useEffect, useState } from 'react';
 import styles from './admins.module.css';
 
 function Admins() {
+  const [admins, saveAdmins] = useState([]);
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API}/admins`)
+      .then((response) => response.json())
+      .then((response) => {
+        saveAdmins(response.data);
+      });
+  }, []);
+
   return (
     <section className={styles.container}>
       <h2>Admins</h2>
+      <div>
+        {admins.map((admin) => {
+          return <div key={admin._id}>{admin.name}</div>;
+        })}
+      </div>
     </section>
   );
 }
