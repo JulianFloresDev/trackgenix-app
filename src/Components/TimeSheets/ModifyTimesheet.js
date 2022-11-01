32bec812c90d11aa6ceed1952646ed142cacf72e
Dino Stampella <dino.stampella96@gmail.com>
2022-11-01 16:36:07 -0300

    TG-09: change some input tags to select tags

--- src/Components/TimeSheets/ModifyTimesheet.js
+++ src/Components/TimeSheets/ModifyTimesheet.js
@@ -1,7 +1,28 @@
 import styles from './time-sheets.module.css';
 import { useState } from 'react';
+import { useEffect } from 'react';
+import Option from './Option';
 
 function modifyTimesheet(props) {
+  const [employeesList, setEmployeesList] = useState([]);
+  const [projectsList, setProjectsList] = useState([]);
+  const [tasksList, setTasksList] = useState([]);
+  useEffect(async () => {
+    try {
+      const resEmployees = await fetch(`${process.env.REACT_APP_API_URL}/employees`);
+      const dataEmployees = await resEmployees.json();
+      setEmployeesList(dataEmployees.data);
+      const resProjects = await fetch(`${process.env.REACT_APP_API_URL}/projects`);
+      const dataProjects = await resProjects.json();
+      setProjectsList(dataProjects.data);
+      const resTasks = await fetch(`${process.env.REACT_APP_API_URL}/tasks`);
+      const dataTasks = await resTasks.json();
+      setTasksList(dataTasks.data);
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
   const [data, setData] = useState({
     date: '',
     description: '',
@@ -16,6 +37,7 @@ function modifyTimesheet(props) {
     const targetName = e.target.name;
     data[targetName] = e.target.value;
     setData({ ...data });
+    console.log(data);
   };
 
   const onEdit = (e) => {
@@ -45,23 +67,50 @@ function modifyTimesheet(props) {
           </div>
           <div>
             <label>Description</label>
-            <input type="text" name="description" value={data.description} onChange={onchange} />
+            <input
+              type="text"
+              name="description"
+              value={data.description}
+              onChange={onchange}
+              placeholder="Description..."
+            />
           </div>
           <div>
             <label>Employee</label>
-            <input type="text" name="employee" value={data.employee} onChange={onchange} />
+            <select name="employee" onChange={onchange}>
+              <option>Select an Employee</option>
+              {employeesList.map((employee) => {
+                return <Option.OptionEmployees key={employee._id} employee={employee} />;
+              })}
+            </select>
           </div>
           <div>
             <label>Project</label>
-            <input type="text" name="project" value={data.project} onChange={onchange} />
+            <select name="project" onChange={onchange}>
+              <option>Select a Project</option>
+              {projectsList.map((project) => {
+                return <Option.OptionProjects key={project._id} project={project} />;
+              })}
+            </select>
           </div>
           <div>
             <label>Task</label>
-            <input type="text" name="task" value={data.task} onChange={onchange} />
+            <select name="task" onChange={onchange}>
+              <option>Select a Task</option>
+              {tasksList.map((task) => {
+                return <Option.OptionTasks key={task._id} task={task} />;
+              })}
+            </select>
           </div>
           <div>
             <label>Hours</label>
-            <input type="number" name="hours" value={data.hour} onChange={onchange} />
+            <input
+              type="number"
+              name="hours"
+              value={data.hour}
+              onChange={onchange}
+              placeholder="Hours"
+            />
           </div>
           {props.errorMessage && <h4>{props.errorMessage.toUpperCase()}</h4>}
           {props.modifyModalControl.id ? (
