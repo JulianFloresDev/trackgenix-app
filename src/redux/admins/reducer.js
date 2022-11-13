30ce400ef654934990608639b7a4d4d7d1edf829
Julian Flores <julianfloresdev@gmail.com>
2022-11-13 17:52:30 -0300

    TG-43: add GET admins reducer

--- src/redux/admins/reducer.js
+++ src/redux/admins/reducer.js
@@ -1,5 +1,34 @@
-const adminsReducer = (state = null) => {
-  return state;
+import {
+  GET_ADMINS_PENDING
+  // GET_ADMINS_SUCCESS,
+  // GET_ADMINS_ERROR,
+  // PUT_ADMINS_PENDING,
+  // PUT_ADMINS_SUCCESS,
+  // PUT_ADMINS_ERROR,
+  // POST_ADMINS_PENDING,
+  // POST_ADMINS_SUCCESS,
+  // POST_ADMINS_ERROR,
+  // DELETE_ADMINS_PENDING,
+  // DELETE_ADMINS_SUCCESS,
+  // DELETE_ADMINS_ERROR
+} from './constants';
+
+const INITIAL_STATE = {
+  list: [],
+  isFetching: false,
+  error: ''
+};
+
+const adminsReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case GET_ADMINS_PENDING:
+      return {
+        ...state,
+        isFetching: true
+      };
+    default:
+      return state;
+  }
 };
 
 export default adminsReducer;
