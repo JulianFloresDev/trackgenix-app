5bd92b28908fc3f74616c740de9b28dd463104ce
kolodani <danielvinzia@gmail.com>
2022-11-15 15:45:09 -0300

    TG-45: delete comments

--- src/Components/SuperAdmins/index.js
+++ src/Components/SuperAdmins/index.js
@@ -1,5 +1,4 @@
 import styles from './super-admins.module.css';
-// import { useEffect } from 'react';
 import Table from '../Share/Table';
 import Spinner from '../Share/Spinner';
 import { useSelector, useDispatch } from 'react-redux';
@@ -13,12 +12,6 @@ function SuperAdmins() {
     dispatch(getSuperAdmins(''));
   }, []);
 
-  // useEffect(async () => {
-  //   const response = await fetch(`${process.env.REACT_APP_API_URL}/super-admins`);
-  //   const data = await response.json();
-  //   setList(data.data || []);
-  //   setTimeout(() => setIsFetching(false), 2000);
-  // }, []);
   return (
     <section className={styles.container}>
       {isFetching ? (
