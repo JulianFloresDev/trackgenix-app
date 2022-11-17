b73a7d861cf4132ede336292c7d5171c4a358b8f
Julian Flores <julianfloresdev@gmail.com>
2022-11-17 16:11:37 -0300

    TG-36: resolve fetching error styles

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -2,35 +2,8 @@ import styles from './header.module.css';
 
 function Header() {
   return (
-    <header>
-      {/* <div className={styles.container}>
-        <div className={styles.brand}>Radium Rocket</div>
-        <div>
-          <a href={'https://www.facebook.com/radiumrocket'} target={'_blank'} rel="noreferrer">
-            <img
-              className={styles.socialIcon}
-              src={`${process.env.PUBLIC_URL}/assets/images/facebook.svg`}
-            />
-          </a>
-          <a href={'https://twitter.com/radiumrocket'} target={'_blank'} rel="noreferrer">
-            <img
-              className={styles.socialIcon}
-              src={`${process.env.PUBLIC_URL}/assets/images/twitter.svg`}
-            />
-          </a>
-          <a href={'https://www.instagram.com/radium.rocket/'} target={'_blank'} rel="noreferrer">
-            <img
-              className={styles.socialIcon}
-              src={`${process.env.PUBLIC_URL}/assets/images/instagram.svg`}
-            />
-          </a>
-        </div>
-        <figure>Here should be an image of who is logged</figure>
-      </div> */}
+    <header className={styles.header}>
       <nav className={styles.navbar}>
-        {/* <div className={styles.appName}>
-          Track<span>GENIX</span>
-        </div> */}
         <div className={styles.logoRR}>
           <img src={`${process.env.PUBLIC_URL}/assets/images/logo-RR.svg`} />
           <img src={`${process.env.PUBLIC_URL}/assets/images/sub-logo-RR.svg`} />
