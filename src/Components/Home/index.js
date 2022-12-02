061a2e00dd3571e37e8bd34ea6ef099b066d9089
Julian Flores <julianfloresdev@gmail.com>
2022-12-02 15:42:27 -0300

    TG-74: delete comment lines

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -3,7 +3,6 @@ import styles from './home.module.css';
 function Home() {
   return (
     <div className={styles.container}>
-      {/* <h1>Welcome to TRACKGENIX!!!</h1> */}
       <main>
         <section className={styles.hero}>
           <div className={styles.leftConteiner}>
