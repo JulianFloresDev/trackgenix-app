2dfeb292a10619959b502551c53ca0329c272735
Julian Flores <julianfloresdev@gmail.com>
2022-12-05 15:54:17 -0300

    TG-77: refactor nav component with styles

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,38 +1,34 @@
 import styles from './header.module.css';
-import { Navbar } from 'Components/Share';
+import { Navbar, LandingNavBar } from 'Components/Share';
 import { useSelector } from 'react-redux';
 
 function Header() {
-  const { authenticated, role } = useSelector((state) => state.auth);
+  const { authenticated, role, isLoading } = useSelector((state) => state.auth);
   return (
-    <header className={styles.header}>
-      <nav className={styles.navbar}>
-        {!authenticated && <Navbar />}
-        {authenticated && (
-          <a className={styles.logoRR} href="/">
-            <img src={`${process.env.PUBLIC_URL}/assets/images/logo-RR.svg`} />
-            <img src={`${process.env.PUBLIC_URL}/assets/images/sub-logo-RR.svg`} />
-          </a>
-        )}
-        {role === 'super-admin' && (
-          <Navbar
-            navOptions={[
-              'super-admins',
-              'admins',
-              'employees',
-              'projects',
-              'time-sheets',
-              'tasks',
-              'profile'
-            ]}
-          />
-        )}
-        {role === 'admin' && (
-          <Navbar navOptions={['admins', 'employees', 'projects', 'time-sheets', 'profile']} />
-        )}
-        {role === 'employee' && <Navbar navOptions={['time-sheets', 'projects', 'profile']} />}
-      </nav>
-    </header>
+    <>
+      {isLoading ? null : (
+        <header className={styles.header}>
+          {!authenticated && <LandingNavBar />}
+          {role === 'super-admin' && (
+            <Navbar
+              navOptions={[
+                'super-admins',
+                'admins',
+                'employees',
+                'projects',
+                'time-sheets',
+                'tasks',
+                'profile'
+              ]}
+            />
+          )}
+          {role === 'admin' && (
+            <Navbar navOptions={['admins', 'employees', 'projects', 'time-sheets', 'profile']} />
+          )}
+          {role === 'employee' && <Navbar navOptions={['time-sheets', 'projects', 'profile']} />}
+        </header>
+      )}
+    </>
   );
 }
 
--- src/Components/Share/Navbar/index.jsx
+++ src/Components/Share/Navbar/index.jsx
@@ -10,96 +10,38 @@ const Navbar = ({ navOptions }) => {
 
   return (
     <>
-      <div className={styles.flexContainer}>
-        {authenticated && (
-          <ul className={styles.rutes}>
-            {navOptions?.map((endPoint, index) => {
-              return (
-                <li key={index}>
-                  <a href={endPoint === 'profile' ? `/${role}s/` : `/${endPoint}`}>
-                    {endPoint.toUpperCase()}
-                  </a>
-                </li>
-              );
-            })}
-          </ul>
-        )}
-        {!authenticated && (
-          <div className={styles.navBarLoggedOut}>
-            <div className={styles.containerLoggedOut}>
-              <h2>Menu</h2>
-              <ul className={styles.rutes}>
-                <li>
-                  <a href={'/home'} className={styles.navbarBtn}>
-                    Home
-                  </a>
-                </li>
-                <li>
-                  <a href={'/auth/sign-up'} className={styles.navbarBtn}>
-                    Sign Up
-                  </a>
-                </li>
-                <li>
-                  <a href={'/auth/login'} className={styles.navbarBtn}>
-                    Log In
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className={styles.navbarBtn}>
-                    Contact Info
-                  </a>
-                </li>
-              </ul>
+      {authenticated && (
+        <>
+          <a className={styles.logoRR} href="/">
+            <img src={`${process.env.PUBLIC_URL}/assets/images/logo-RR.svg`} />
+            <img src={`${process.env.PUBLIC_URL}/assets/images/sub-logo-RR.svg`} />
+          </a>
+          <nav className={styles.flexContainer}>
+            <ul className={styles.rutes}>
+              {navOptions?.map((endPoint, index) => {
+                return (
+                  <li key={index}>
+                    <a href={endPoint === 'profile' ? `/${role}s/` : `/${endPoint}`}>
+                      {endPoint.toUpperCase()}
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+            <div className={styles.buttonsContainer}>
+              <button
+                className={styles.buttonItem}
+                onClick={() => {
+                  dispatch(logout());
+                  history.push('/');
+                }}
+              >
+                Log Out
+              </button>
             </div>
-            <div className={styles.containerLoggedOut}>
-              <h2>Get in touch</h2>
-              <p>
-                Gigatech is always looking for talented software developers to join our team. If you
-                are interested in working with us, please get in touch. We would be happy to discuss
-                your experience and skills.
-              </p>
-            </div>
-            <div className={styles.containerContactInfo}>
-              <div>
-                <img src={`${process.env.PUBLIC_URL}/assets/images/envelope.svg`} alt="" />
-                <a href="">trackgenix@gigatech.com</a>
-              </div>
-              <div>
-                <img src={`${process.env.PUBLIC_URL}/assets/images/phone.svg`} alt="" />
-                <a href="">+598 99123456</a>
-              </div>
-              <div>
-                <img src={`${process.env.PUBLIC_URL}/assets/images/home.svg`} alt="" />
-                <a href="">Av. Montevideo 1234, 11200</a>
-              </div>
-            </div>
-          </div>
-          // <div className={styles.logginOptions}>
-          //   <button
-          //     className={styles.buttonItem}
-          //     onClick={() => {
-          //       history.push('/auth/login');
-          //     }}
-          //   >
-          //     Log In
-          //   </button>
-          //   <a href={'/auth/sign-up'}>{`Don't have account? Create one here.`}</a>
-          // </div>
-        )}
-        {authenticated && (
-          <div className={styles.buttonsContainer}>
-            <button
-              className={styles.buttonItem}
-              onClick={() => {
-                dispatch(logout());
-                history.push('/');
-              }}
-            >
-              Log Out
-            </button>
-          </div>
-        )}
-      </div>
+          </nav>
+        </>
+      )}
     </>
   );
 };
