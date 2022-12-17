487d7b52b15633f4f53bdbaa88aba5d1981fe760
Dino Stampella <dino.stampella96@gmail.com>
2022-12-17 01:00:23 -0300

    TG-93: user container in navbar

--- src/Components/Share/Navbar/index.jsx
+++ src/Components/Share/Navbar/index.jsx
@@ -1,19 +1,29 @@
 import styles from './navbar.module.css';
 import { useHistory } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { logout } from 'redux/auth/thunks';
 
 const Navbar = ({ navOptions }) => {
   const dispatch = useDispatch();
   const history = useHistory();
   const rute = history.location.pathname.slice(1);
+  const { user } = useSelector((state) => state.global);
+  const { role } = useSelector((state) => state.auth);
 
   return (
     <nav className={styles.flexContainer}>
-      <a className={styles.logoRR} href="/">
+      {/* <a className={styles.logoRR} href="/">
         <img src={`${process.env.PUBLIC_URL}/assets/images/logo-RR.svg`} />
         <img src={`${process.env.PUBLIC_URL}/assets/images/sub-logo-RR.svg`} />
-      </a>
+      </a> */}
+      <div className={styles.userContainer}>
+        <img
+          className={styles.userPicture}
+          src={`${process.env.PUBLIC_URL}/assets/images/user-picture.svg`}
+        />
+        <h3 className={styles.userName}>{`${user.firstName} ${user.lastName}`}</h3>
+        <h4 className={styles.userRole}>{role}</h4>
+      </div>
       <ul className={styles.rutes}>
         {navOptions?.map((endPoint, index) => {
           return (
