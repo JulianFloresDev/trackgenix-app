87dee172fa18b420e2e816c2dd78873f33c14149
kolodani <danielvinzia@gmail.com>
2022-12-20 16:43:50 -0300

    TG-99: fix submit time-sheet error

--- src/Components/Share/Form/index.js
+++ src/Components/Share/Form/index.js
@@ -126,7 +126,7 @@ const Form = () => {
         employee: itemToPUT.employee?._id || itemToPUT.employee,
         task: itemToPUT.task?._id || itemToPUT.task,
         project: itemToPUT.project?._id || itemToPUT.project,
-        employeePM: itemToPUT.employeePM?.toString() || '0',
+        employeePM: itemToPUT.employeePM?.toString(),
         teamMembers: itemToPUT.teamMembers?.map((member) => {
           return { ...member, employee: member.employee?._id || member.employee };
         })
