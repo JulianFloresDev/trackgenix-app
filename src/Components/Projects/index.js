60a3510a2094b127e085090c13a311a7138beaae
Julian Flores <julianfloresdev@gmail.com>
2022-12-20 22:44:29 -0300

    TG-99: replace email to user in project component

--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -7,10 +7,9 @@ import { Table, Spinner } from 'Components/Share';
 
 function Projects() {
   const dispatch = useDispatch();
-  const { role } = useSelector((store) => store.auth);
+  const { role, email } = useSelector((store) => store.auth);
   const { list: projectsList, isFetching, error } = useSelector((store) => store.projects);
   const { list: timesheetList } = useSelector((store) => store.timeSheets);
-  const { user } = useSelector((state) => state.global);
   useEffect(async () => {
     dispatch(getProjects(''));
     dispatch(getTimesheets(''));
@@ -40,13 +39,13 @@ function Projects() {
                 role === 'employee'
                   ? projectsList.filter(
                       (project) =>
-                        project.employeePM?.employee?._id === user._id ||
-                        project.teamMembers?.find((member) => member.employee?._id === user._id)
+                        project.employeePM?.employee?.email === email ||
+                        project.teamMembers?.find((member) => member.employee?.email === email)
                     )
                   : projectsList
               }
               filteredTimesheets={
-                role === 'employee' && timesheetList?.filter((ts) => ts.employee?._id === user._id)
+                role === 'employee' && timesheetList?.filter((ts) => ts.employee?.email === email)
               }
               editable={role === 'admin' && { edit: true, remove: true, add: true }}
             />
