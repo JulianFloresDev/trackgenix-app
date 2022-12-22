4cbf18484d61f14d88b627d93489e0b00ca1c85a
Julian Flores <julianfloresdev@gmail.com>
2022-12-21 23:36:11 -0300

    hotfix: fix employee table when role is admin

--- src/Components/Employees/index.js
+++ src/Components/Employees/index.js
@@ -6,6 +6,7 @@ import { Table, Spinner, NotFound } from 'Components/Share';
 
 function Employees() {
   const { list, isFetching, error } = useSelector((state) => state.employees);
+  const { role } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getEmployees(''));
@@ -20,6 +21,7 @@ function Employees() {
         <Table
           headers={['firstName', 'lastName', 'dni', 'email', 'location', 'phone']}
           data={list}
+          editable={role === 'admin' && { add: false, remove: true, edit: true }}
         />
       )}
     </section>
