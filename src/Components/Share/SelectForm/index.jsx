623eab0baa8d0ac4ed3fe814e7d382b158d007bf
Julian Flores <julianfloresdev@gmail.com>
2022-12-21 23:12:06 -0300

    sort selects in Form component and TeamMembers Table

--- src/Components/Share/SelectForm/index.jsx
+++ src/Components/Share/SelectForm/index.jsx
@@ -10,14 +10,26 @@ const SelectForm = ({ register, element, label, selectOptions, error }) => {
       )}
       <select {...register(element)} className={styles.flexSelect} id={element} name={element}>
         <option hidden>{`Select ${element}`}</option>
-        {selectOptions.map((option, index) => {
-          return (
-            <option key={index} value={option?._id}>
-              {option?.firstName && option.firstName + ' ' + option.lastName}
-              {(option?.name && option.name) || option?.description || option?.type}
-            </option>
-          );
-        })}
+        {selectOptions
+          .sort((a, b) => {
+            if (a.firstName) {
+              return a?.firstName > b?.firstName ? 1 : a?.firstName < b?.firstName ? -1 : 0;
+            }
+            if (a.name) {
+              return a?.name > b?.name ? 1 : a?.name < b?.name ? -1 : 0;
+            }
+            if (a.type) {
+              return a?.type > b?.type ? 1 : a?.type < b?.type ? -1 : 0;
+            }
+          })
+          .map((option, index) => {
+            return (
+              <option key={index} value={option?._id}>
+                {option?.firstName && option.firstName + ' ' + option.lastName}
+                {(option?.name && option.name) || option?.description || option?.type}
+              </option>
+            );
+          })}
       </select>
       {error && Array.isArray(error) ? (
         <ul className={styles.errorMessage}>
